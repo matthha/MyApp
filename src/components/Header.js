f1524b2dc48113913524e3f4683ce894787c908e
matthha <122128508+matthha@users.noreply.github.com>
2023-05-15 15:03:18 +0000

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+const Header = () => {
+
+    return (
+        <div className="grid">
+        
+        <h1>THings</h1>
+        <img width="198" height="36" src="https://nexus.engin.umich.edu/wp-content/uploads/2022/10/nexus-ME-logo-formal.svg" className="custom-logo astra-logo-svg entered lazyloaded" alt="Nexus at Michigan Engineering" decoding="async" data-lazy-src="https://nexus.engin.umich.edu/wp-content/uploads/2022/10/nexus-ME-logo-formal.svg" data-ll-status="loaded"></img>
+        </div>
+    )
+
+}
+export default Header;
